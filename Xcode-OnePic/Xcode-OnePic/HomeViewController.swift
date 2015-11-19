471a655392d470baef4ebc02bb74c7b2be41cfde
dingkais <kaiding@kaiding-mbp.local>
2015-11-19 00:45:05 -0800

    Add camera function WIP

--- Xcode-OnePic/Xcode-OnePic/HomeViewController.swift
+++ Xcode-OnePic/Xcode-OnePic/HomeViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
    
+    @IBOutlet var cameraImageView: UIView!
+    
+    var imagePicker: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,6 +29,18 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func takePhoto(sender: UIButton) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+//    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+//        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+//        cameraImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+//    }
 
     /*
     // MARK: - Navigation
